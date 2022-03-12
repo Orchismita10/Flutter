a7e114815d437bd7fa006cd814e74c830832c05c
Orchismita10 <99389659+Orchismita10@users.noreply.github.com>
2022-03-12 21:58:43 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: const Center(
+            child: Text(' My 1st App ')
+        )
+        )
+      )
+    )
+
+  );
+
+}
+
